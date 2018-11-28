51b40cd4ce7a8eb6c597be3219ae9d0f56b3d04e
Luke Welton <luke.welton97@gmail.com>
2018-11-28 15:57:32 -0600

    report average score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from A_Star import greedy
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
 from math import log2
 
@@ -7,9 +7,11 @@ from math import log2
 if __name__ == "__main__":
     runs = 100
     max_tiles = []
+    total_score = 0
 
     for _ in range(runs):
         score, max_tile = greedy()
+        total_score += score
         max_tiles.append(max_tile)
 
     x_values = []
@@ -21,15 +23,13 @@ if __name__ == "__main__":
     for tile in max_tiles:
         counts[max(int(log2(tile / 12)), 0)] += 1
 
-    ind = numpy.arange(len(x_values))
-    p = plt.bar(ind, counts)
+    ind = np.arange(len(x_values))
+
+    plt.bar(ind, counts)
     plt.title("Greedy Playing Threes!")
     plt.xlabel("Highest Tile Achieved")
     plt.ylabel("Count")
     plt.xticks(ind, x_values)
-    plt.yticks(numpy.arange(0, max(counts), 10))
     plt.show()
 
-
-
-
+    print("Average Score: {}".format(total_score / runs))
