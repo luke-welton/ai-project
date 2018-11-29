dabf0ac930cafc56d119b139182b8ce0c711bee0
kinztechcom <allen@kinztech.com>
2018-11-28 23:16:52 -0600

    average & highest tiles bar graphs

--- montecarlo.py
+++ montecarlo.py
@@ -1,4 +1,5 @@
 import random, time
+import numpy as np
 from threes import Board, Directions, Tile, InvalidMove
 from matplotlib import pyplot as plt
 
@@ -90,14 +91,18 @@ class MonteCarlo:
         plt.title('Predicted Score vs Actual for Depth ' + str(self.depth))
         plt.ylabel('Score')
         plt.xlabel('Moves')
-        plt.show()
+        #plt.show()
+        plt.savefig(str(self.depth) + '_converge.png')
         return True
 
 # Collect data on the MonteCarlo implementation.
-def run_monte_carlo(runs=[100, 50, 10, 5, 1], depths=[50, 100, 500, 1000, 5000],debug=False, print_results=True, save=False):
+def run_monte_carlo(runs=[100, 50, 10, 5, 1], depths=[50, 100, 500, 1000, 5000], debug=False, print_results=True, save=False):
     scores = {}
     moves = {}
     tiles = {}
+    avg_scores = [0] * len(depths)
+    min_tiles = [0] * len(depths)
+    max_tiles = [0] * len(depths)
 
     # Set empty arrays in dictionaries
     for depth in depths:
@@ -109,32 +114,57 @@ def run_monte_carlo(runs=[100, 50, 10, 5, 1], depths=[50, 100, 500, 1000, 5000],
     mc = MonteCarlo(debug=debug, print_results=print_results)
     for index, depth in enumerate(depths):
         mc.depth = depth
-        mc.runs = 0
+        mc.run = 0
+        avg_score = 0
+        min_tile = 100000
+        max_tile = 0
         start = int(round(time.time() * 1000))
         for i in range(runs[index]):
             mc.start()
             scores[depth].append(mc.score)
+            avg_score += mc.score
             moves[depth].append(mc.moves)
             tiles[depth].append(mc.highest_tile)
-            #mc.graph_results()
+            if mc.highest_tile < min_tile:
+                min_tile = mc.highest_tile
+            if mc.highest_tile > max_tile:
+                max_tile = mc.highest_tile
+            mc.graph_results()
         end = int(round(time.time() * 1000))
         elapsed = (end - start) / 1000
-        print ('[' + str(mc.depth) + '] Time taken: ' + str(elapsed) + ' seconds', save)
-        graph_results(1, scores[depth], 'Playout Score for Depth ' + str(mc.depth), 'Score', save)
-        graph_results(2, moves[depth], '# of Moves for Depth ' + str(mc.depth), 'Move', save)
-        graph_results(3, tiles[depth], 'Highest Tile for Depth ' + str(mc.depth), 'Highest Tile', save)
+        avg_score /= runs[index]
+        avg_scores[index] = avg_score
+        min_tiles[index] = min_tile
+        max_tiles[index] = max_tile
+        if print_results:
+            print ('[' + str(mc.depth) + '] Time taken: ' + str(elapsed) + ' seconds')
+            print ('[' + str(mc.depth) + '] Average score: ' + str(avg_scores[index]) + ' | Min tile: ' + str(min_tiles[index]) + ' | Max tile: ' +str(max_tiles[index]))
+        graph_results(mc.depth + 1, scores[depth], 'Playout Score for Depth ' + str(mc.depth), 'Score', save)
+        graph_results(mc.depth + 2, moves[depth], '# of Moves for Depth ' + str(mc.depth), 'Move', save)
+        graph_results(mc.depth + 3, tiles[depth], 'Highest Tile for Depth ' + str(mc.depth), 'Highest Tile', save)
+
+    box_results(mc.depth + 4, avg_scores, depths, 'Average Scores for Depths', 'Average Score')
+    box_results(mc.depth + 5, max_tiles, depths, 'Highest Tiles for Depths', 'Tile')
 
 def graph_results(run, data, title, ytitle, save=False):
-        plt.figure(run)
-        plt.plot(data)
-        plt.title(title)
-        plt.ylabel(ytitle)
-        plt.xlabel('Trial #')
-        if save:
-            plt.savefig(title.lower().replace(' ', '_') + ".png")
-        else:
-            plt.plot()
+    plt.figure(run)
+    plt.plot(data)
+    plt.title(title)
+    plt.ylabel(ytitle)
+    plt.xlabel('Trial #')
+    if save:
+        plt.savefig(title.lower().replace(' ', '_') + ".png")
+    else:
+        plt.plot()
 
+def box_results(run, data, depths, title, ylabel):
+    plt.figure(run)
+    y_pos = np.arange(len(depths))
+    plt.bar(y_pos, data, align='center')
+    plt.xticks(y_pos, depths)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.savefig(title.lower().replace(' ', '_') + ".png")
 
 # Here we go.     
-run_monte_carlo(runs=[2], depths=[50], debug=True, save=True)
+run_monte_carlo(runs=[1], depths=[1000], save=True, debug=True)
